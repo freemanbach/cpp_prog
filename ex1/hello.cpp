66c76ee176108c59b9647f1a267e4fa131926aff
freeman lo <flo@radford.edu>
2023-04-16 19:20:01 -0400

    Regular Compile with makefile

---
+++ ex1/hello.cpp
@@ -0,0 +1,19 @@
+/*
+ * @file
+ * @author [freemanbach](https://github.com/freemanbach
+ * @brief Simple C++ code
+ *
+ */
+
+#include <algorithm>  /// For std::copy
+#include <array>      /// For std::array
+#include <cassert>    /// For assert
+#include <cstring>    /// For std::memcopy
+#include <iostream>   /// For IO operations
+#include <string>     /// For strings
+#include <vector>     /// For std::vector
+
+int main() {
+    std::cout << "Hello World!\n";
+    return 0;
+}
