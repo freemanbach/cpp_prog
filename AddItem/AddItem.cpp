4b780c825b998d078149554b818f9301513946d4
freemanbach <flo@radford.edu>
2023-10-27 14:47:19 -0400

    testing with C++ prog lang

---
+++ AddItem/AddItem.cpp
@@ -0,0 +1,18 @@
+// *************************************************
+// Author : Freeman
+// email  : 
+// Desc   : This file contains the 'main' function. 
+//          Program execution begins and ends there.
+// Date   : 20231027
+// *************************************************
+#include <iostream>
+#include "md5.h"
+
+using namespace std;
+
+int main(){
+    //std::cout << "Hello World!\n";
+    hashing::md5::test();
+    std::cin.get();
+    return 0;
+}
