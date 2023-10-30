3c63fc9cb030fea449fb9f88a78c9773d80df890
freemanbach <flo@radford.edu>
2023-10-29 20:24:45 -0400

    added code to test for performance.

--- AddItem/AddItem.cpp
+++ AddItem/AddItem.cpp
@@ -3,18 +3,37 @@
 // email  : 
 // Desc   : This file contains the 'main' function. 
 //          Program execution begins and ends there.
-// Date   : 20231027
+// Date   : 20231028
 // *************************************************
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <chrono>
 #include "md5.h"
 #include "factorial.h"
+#include "sha256.h"
 
 using namespace std;
 
 int main(){
-    //std::cout << "Hello World!\n";
-    math::tests();
-    hashing::md5::test();
-    std::cin.get();
+    string filename = "C:/Users/flo1/httpd.conf";
+    string data = "";
+    string csum = "";
+    std::ifstream afile; 
+    afile.open(filename);
+    auto start = chrono::high_resolution_clock::now();
+    if (afile.is_open()) {
+        while (afile.good()) {
+            afile >> data;
+        }
+      csum = hashing::sha256::sha256(data);
+       cout << "*" << csum << "    " << filename << endl;
+    }
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+    cout << duration.count()<< endl;
+
+    // cin.get();
     return 0;
 }
